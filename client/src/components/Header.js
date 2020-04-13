1f41c4de540b59afc8d06f0a97c631ac195774a5
MaryArbs <mary.arbachauskas@gmail.com>
2020-04-13 14:23:44 -0400

    update how Categories render

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,7 +4,7 @@ import React from 'react'
 const Header = () => 
 
     <div className="MainHeader">
-        <h1 style={{textAlign: 'center'}}>— LET'S CONNECT—</h1>
+        <h1 style={{textAlign: 'center'}}>LET'S CONNECT</h1>
     </div>
     
     
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,14 +3,10 @@ import { Container } from 'semantic-ui-react'
 import './Home.css'
 
 const Home = () =>
-
-  <div>
-    <Container className="container">
+    
     <div className="quote">
         <h2>"Coming together is a beginning; keeping together is progress; working together is success." - Henry Ford</h2>
-     </div>
-    </Container>
-  </div>
+    </div>
 
 
 export default Home;
